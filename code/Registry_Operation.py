6b80a94d96a49063fb9eb9523529931148624934
이준하 <>
2020-02-06 16:14:01 +0900

    registry operation - querykey, enumkey, enumvalue

--- code/Registry_Operation.py
+++ code/Registry_Operation.py
@@ -1,6 +1,8 @@
 import json
 
 '''
+Structure
+
 1.	QueryKey
     Input : Key
     Output : # of subkeys / value pairs
@@ -21,7 +23,7 @@ import json
 
 def openkey(key):
 
-    with open('/Users/junha/Documents/Junha/Study/Bigbase/Registry_MapReduce/data/HKEY_CLASSES_ROOT.json') as json_file: 
+    with open('/Users/junha/Documents/Junha/Study/Bigbase/Registry_MapReduce/data/sample.json') as json_file: 
         data = json.load(json_file) 
 
     keys = key.split('\\')
@@ -32,27 +34,74 @@ def openkey(key):
     return data
 
 
-def queryvalue(data, value_name):
+def querykey(key):
 
-    value_data = data[value_name]
+    subkey = []
+    value_pairs = []
+
+    for k in key:
+        if type(sample[k]) == dict:
+            subkey.append(k)
+        elif type(key[k]) == str:
+            value_pairs.append({k:key[k]})
+    
+    num_of_subkeys = len(subkey)
+
+    return num_of_subkeys, value_pairs
+
+
+
+def queryvalue(key, value_name):
+
+    value_data = key[value_name]
 
     return value_data
 
 
-def querykey(data):
 
-    return data
+def enumkey(key):
 
-def enumkey(data):
+    subkey = []
 
-    return data
+    for k in key:
+        if type(key[k]) == dict:
+            subkey.append(k)
 
-def enumvalue(data):
-
-    return data
-
+    return subkey
 
 
 
-value = queryvalue(openkey('HKEY_CLASSES_ROOT\\*'), 'ConflictPrompt')
-print(value)
+def enumvalue(key):
+
+    value_pairs = []
+
+    for k in key:
+        if type(key[k]) == str:
+            value_pairs.append({k:key[k]})
+
+    return value_pairs
+
+
+
+if __name__ == "__main__":
+
+    key = 'HKEY_CLASSES_ROOT\\*'
+    value_name = 'ConflictPrompt'
+
+    #openkey -> querykey
+    print(querykey(openkey(key))[0])
+    print(querykey(openkey(key))[1])
+
+    #openkey -> queryvalue -> closekey
+    print(queryvalue(openkey(key), value_name))
+
+    #openkey -> enumkey
+    print(enumkey(openkey(key)))
+
+
+    #openkey -> enumvalue
+    print(enumvalue(openkey(key)))
+
+
+
+
