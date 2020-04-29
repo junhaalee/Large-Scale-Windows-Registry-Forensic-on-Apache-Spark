c87a46b5e79e815caf80e4aeb750b20faaeaf16b
이준하 <>
2020-04-29 18:52:05 +0900

    1. reg2json 수정 2. forensic_pyspark 추가 - key/value update 탐지

--- code/Reg2Json.py
+++ code/Reg2Json.py
@@ -1,5 +1,6 @@
 import os
 import json
+import io
 
 def list_files_subdir(destpath, ext):
     
@@ -24,13 +25,13 @@ def save_value(values):
         else:
             value_name = value.split('=')[0][1:-1]
 
-        if value.split('=')[1][1] == '"':
+        if value.split('=')[1][0] == '"':
             value_data = value.split('=')[1][1:-1]
         else:
-            value_data = value.split('=')[1][1:-1]
+            value_data = value.split('=')[1]
 
         if len(value_data) == 0:
-            pair = {value_name : ' '}
+            pair = {value_name : 'none'}
         else:
             pair = {value_name : value_data}
 
@@ -41,30 +42,40 @@ def save_value(values):
 
 def save_as_json(file_list,path):
 
+    file_name = file_list[4]
+
     for file_name in file_list:
 
         reg = {}
 
         try:
-            with open(file_name, 'r') as f:
+            with io.open(file_name, 'r') as f:
                 data = f.read().split("\n\n")[1:]
         except:
-            with open(file_name, 'r', encoding='utf-16') as f:
+            with io.open(file_name, 'r', encoding='utf-16') as f:
                 data = f.read().split("\n\n")[1:]
+
+        sample = data[157]
 
         for sample in data:
 
-            keys = sample.split('\n')[0][1:-1].split('\\')
+            keys = map(str,sample.split('\n')[0][1:-1].split('\\'))
 
             try:
-                values = sample.split('\n')[1:]
+                values = map(str,sample.split('\n')[1:])
             except:
                 pass
             
             #value_data가 여러줄에 걸쳐있는 경우 하나의 value_data로 만들어주기
-            for i in range(len(values)-1,0,-1):
-                if '=' not in values[i]:
-                    values[i-1] += values[i]
+            ind = 0
+            while(True):
+                if ind == len(values):
+                    break
+                if values[ind][-1:] == '\\':
+                    values[ind] = values[ind][:-2]+str(',')+values[ind+1][2:]     
+                    del values[ind+1] 
+                else: 
+                    ind += 1
             
             ind = 0
             while(True):
@@ -88,12 +99,15 @@ def save_as_json(file_list,path):
                     else:
                         temp = temp[key]
                 except:
-                    print(keys)
+                    pass
+
+
+        final = eval(str(reg).replace('{}',"'none'").replace(',\\  ',','))
         
         name = file_name.split('data/')[1].split('.')[0]
 
         with open(path+'/'+name+'.json','w') as towrite:
-            json.dump(reg,towrite,ensure_ascii=False)
+            json.dump(final,towrite,ensure_ascii=True)
 
 
 if __name__ == "__main__":
